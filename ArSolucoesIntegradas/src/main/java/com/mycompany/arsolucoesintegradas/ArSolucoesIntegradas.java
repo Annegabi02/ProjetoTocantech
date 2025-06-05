b58cfa6e1003a066f23bb4031f84f72684cf6ea0
ALUNOS <annegabriellemirapachecogmail.com>
2025-06-05 15:50:04 -0300

    Atualizando novas telas

---
+++ ArSolucoesIntegradas/src/main/java/com/mycompany/arsolucoesintegradas/ArSolucoesIntegradas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arsolucoesintegradas;
+
+/**
+ *
+ * @author ALUNOS
+ */
+public class ArSolucoesIntegradas {
+
+    public static void main(String[] args) {
+        
+    }
+}
